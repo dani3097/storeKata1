4c34e9d435b131839a7bbca3b6a48f321fe33be0
daniela vela <danielitavo97@gmail.com>
2020-03-03 09:30:14 -0400

    extract method addTax for USA

--- src/main/store/Order.java
+++ src/main/store/Order.java
@@ -61,11 +61,15 @@ public class Order {
 
 		if (this.deliveryCountry == "USA"){
 			// total=totalItems + tax + 0 shipping
-			return totalItems + totalItems * 5 / 100;
+			return totalItems + addTax(totalItems);
 		}
 
 		// total=totalItemst + tax + 15 shipping
-		return totalItems + totalItems * 5 / 100 + 15;
+		return totalItems + addTax(totalItems) + 15;
+	}
+
+	private float addTax(float totalItems) {
+		return totalItems * 5 / 100;
 	}
 
 	private float totalByCategory(OrderItem item) {
