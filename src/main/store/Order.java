30108fdcdf6991d7d45a3402820927dcfa352cc4
daniela vela <danielitavo97@gmail.com>
2020-03-05 08:44:13 -0400

    extract method calculateTotalFor

--- src/main/store/Order.java
+++ src/main/store/Order.java
@@ -55,26 +55,7 @@ public class Order {
 	public float total() {
 		float totalItems = 0;
 		for (OrderItem item : items) {
-			float totalItem=0;
-			float itemAmount = item.getProduct().getUnitPrice() * item.getQuantity();
-			if (item.getProduct().getCategory() == ProductCategory.Accessories) {
-				float booksDiscount = 0;
-				if (itemAmount >= 100) {
-					booksDiscount = itemAmount * 10 / 100;
-				}
-				totalItem = itemAmount - booksDiscount;
-			}
-			if (item.getProduct().getCategory() == ProductCategory.Bikes) {
-				// 20% discount for Bikes
-				totalItem = itemAmount - itemAmount * 20 / 100;
-			}
-			if (item.getProduct().getCategory() == ProductCategory.Cloathing) {
-				float cloathingDiscount = 0;
-				if (item.getQuantity() > 2) {
-					cloathingDiscount = item.getProduct().getUnitPrice();
-				}
-				totalItem = itemAmount - cloathingDiscount;
-			}
+			float totalItem = calculateTotalFor(item);
 			totalItems += totalItem;
 		}
 
@@ -85,5 +66,29 @@ public class Order {
 
 		// total=totalItemst + tax + 15 shipping
 		return totalItems + totalItems * 5 / 100 + 15;
+	}
+
+	private float calculateTotalFor(OrderItem item) {
+		float totalItem=0;
+		float itemAmount = item.getProduct().getUnitPrice() * item.getQuantity();
+		if (item.getProduct().getCategory() == ProductCategory.Accessories) {
+			float booksDiscount = 0;
+			if (itemAmount >= 100) {
+				booksDiscount = itemAmount * 10 / 100;
+			}
+			totalItem = itemAmount - booksDiscount;
+		}
+		if (item.getProduct().getCategory() == ProductCategory.Bikes) {
+			// 20% discount for Bikes
+			totalItem = itemAmount - itemAmount * 20 / 100;
+		}
+		if (item.getProduct().getCategory() == ProductCategory.Cloathing) {
+			float cloathingDiscount = 0;
+			if (item.getQuantity() > 2) {
+				cloathingDiscount = item.getProduct().getUnitPrice();
+			}
+			totalItem = itemAmount - cloathingDiscount;
+		}
+		return totalItem;
 	}
 }
